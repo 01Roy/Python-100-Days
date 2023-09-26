dba4a0590cf969fe8249ba7dcd1dbd57fd53b1ef
Roy01 <subhamroy1180@gmail.com>
2023-09-26 22:37:08 +0530

    few files upload

---
+++ Day10.py
@@ -0,0 +1,5 @@
+# take user input
+# variavle = input();
+
+a = input("Enter your name")
+print(a)
---
+++ Day7.py
@@ -0,0 +1,7 @@
+# Building a calulator
+print(5 + 2)  # for adding
+print(5 - 2)  # for sub
+print(5 * 2)  # for multi
+print(5 / 2)  # for divison
+print(5 // 2)  # remove after decimal
+print(5**2)  # ** this shws the power
