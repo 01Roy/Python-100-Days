b28ddbdb7ea66530d72eba41f7b06dad116074f7
Roy01 <subhamroy1180@gmail.com>
2023-09-26 23:15:24 +0530

    day13

---
+++ Day13.py
@@ -0,0 +1,12 @@
+# String Functions
+
+name = "!!!!Subham!!!!!!!!!!!!"
+
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.rstrip("!"))  # remove from the right side
+print(name.replace("Subham", "Hacker"))
+
+blogHaeding = "introduction to python"
+print(blogHaeding.capitalize())
