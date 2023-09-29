50e9374e7886532ad35654e65bb9c4ebb05a229c
Roy01 <subhamroy1180@gmail.com>
2023-09-29 22:34:21 +0530

    added new file

---
+++ Day14.py
@@ -0,0 +1,13 @@
+# learning If else conditions
+
+a = int(input("Enter your age"))
+
+print("Your age is ", a)
+
+# Condition
+if a > 18:
+    print("You can drive")
+elif a == 18:
+    print("You can't drive")
+else:
+    print("Please enter your correct age")
