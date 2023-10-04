ace741e1c7fd18ed98557230f4c7ee99a20a41e7
Roy01 <subhamroy1180@gmail.com>
2023-10-04 23:32:34 +0530

    day18

---
+++ Day18.py
@@ -0,0 +1,19 @@
+# Learning while  and do while loop
+#  user input
+# i = int(input("Enter the Number"))
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+
+# else use with while loop
+# count = 5
+# while count > 0:
+#     print(count)
+#     count = count - 1
+# else:
+#     print("I am inside else")
+
+# doWhileLoop
+# do {
+# loop body;
+# }while(condition);
