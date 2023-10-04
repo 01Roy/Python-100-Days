d3c6fd2d27c86a3b15b139108541d8a8e2bd9121
Roy01 <subhamroy1180@gmail.com>
2023-10-04 23:13:39 +0530

    Day17

---
+++ Day17.py
@@ -0,0 +1,16 @@
+# learning about for loop
+
+# name = "Subham"
+# for i in name:
+#     print(i)
+
+# colors = ["red", "blue", "green", "yellow"]
+# for color in colors:
+#     print(color)
+
+# using range
+# for k in range(5):
+#     print(k)
+
+for k in range(1, 12, 3):
+    print(k)
